4b2e380278a97514ae7f49e6c48e0f1b9fd62c3d
Marcus Hufvudsson <mh@protohuf.com>
2023-08-21 23:14:00 +0200

    Updated script for creating releases to v2

--- create_release.sh
+++ create_release.sh
@@ -7,4 +7,8 @@ rm -rf $TMPDIR
 rm -f $ARCHIVE
 mkdir $TMPDIR
 cp -rfp Data $TMPDIR
+cp -rfp common $TMPDIR
+cp -rfp sv $TMPDIR
+cp -rfp en $TMPDIR
+cp README.md $TMPDIR
 7zr a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $ARCHIVE $TMPDIR
